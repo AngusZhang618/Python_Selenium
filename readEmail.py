9ab0d0e91d4ea96035cf6fac49a3f2a7f42d49a3
AngusZhang618 <wxfy2000@163.com>
2017-05-16 12:23:12 +0800

    Revert "AlertTest"
    This reverts commit ee9d9753d0be82c0197872bad770d1e03c08133a.

--- readEmail.py
+++ readEmail.py
@@ -1,107 +1,105 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-
 import os
 import email
 
-class ReadEmail():
+#the path of the email saved
+PATH = ''
 
-    #the path of the email saved
-    PATH = ''
+#Read file
+def read_mail(path):
+    if os.path.exists(path):
+        with open(path) as fp:
+            for line in fp:
+                print(line)
+    else:
+        not_find_file()
 
-    #Read file
-    def read_mail(self,path):
-        if os.path.exists(path):
-            with open(path) as fp:
-                for line in fp:
-                    print(line)
-        else:
-            self.not_find_file()
+#Open a file
+def open_file(path):
+    if os.path.exists(path):
+        return open(path,'r')
+    else:
+        not_find_file()
 
-    #Open a file
-    def open_file(self,path):
-        if os.path.exists(path):
-            return open(path,'r')
-        else:
-            self.not_find_file()
+#Create message
+def get_message(path):
+    if os.path.exists(path):
+        fp = open_file(path)
+        return email.message_from_file(fp)
 
-    #Create message
-    def get_message(self,path):
-        if os.path.exists(path):
-            fp = self.open_file(path)
-            return email.message_from_file(fp)
+    else:
+        not_find_file()
 
-        else:
-            self.not_find_file()
+#Get subject
+def get_subject(path):
+    if os.path.exists(path):
+        message = get_message(path)
+        print(type(message))
+        return message.get("subject")
+    else:
+        not_find_file()
 
-    #Get subject
-    def get_subject(self,path):
-        if os.path.exists(path):
-            message = self.get_message(path)
-            return message.get("subject")
-        else:
-            self.not_find_file()
+#解析subject对象
+def parse_subject(msg):
+    if msg != None:
+        subject = msg.get('subject')
+    else:
+        empty_obj()
 
-    #解析subject对象
-    def parse_subject(self,msg):
-        if msg != None:
-            subject = msg.get('subject')
-        else:
-            self.empty_obj()
+#获取发件人信息
+def get_from(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get("from"))[1]
+    else:
+        empty_obj()
 
-    #获取发件人信息
-    def get_from(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get("from"))[1]
-        else:
-            self.empty_obj()
+#获取发件人信息
+def get_to(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get("to"))[1]
+    else:
+        empty_obj()
 
-    #获取发件人信息
-    def get_to(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get("to"))[1]
-        else:
-            self.empty_obj()
+#获取邮件生成时间
+def get_date(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get("date"))[1]
+    else:
+        empty_obj()
 
-    #获取邮件生成时间
-    def get_date(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get("date"))[1]
-        else:
-            self.empty_obj()
+#获取邮件生成版本
+def get_mime_version(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get('mime-version'))[1]
+    else:
+        empty_obj()
 
-    #获取邮件生成版本
-    def get_mime_version(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get('mime-version'))[1]
-        else:
-            self.empty_obj()
+#获取邮件文本类型
+def get_content_type(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get('contect-type'))[1]
+    else:
+        empty_obj()
 
-    #获取邮件文本类型
-    def get_content_type(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get('contect-type'))[1]
-        else:
-            self.empty_obj()
+#获取邮件ID
+def get_message_id(msg):
+    if msg != None:
+        return email.utils.parseaddr(msg.get('message-id'))[1]
+    else:
+        empty_obj()
 
-    #获取邮件ID
-    def get_message_id(self,msg):
-        if msg != None:
-            return email.utils.parseaddr(msg.get('message-id'))[1]
-        else:
-            self.empty_obj()
+#文件不存在
+def not_find_file():
+    print("file not exists!")
 
-    #文件不存在
-    def not_find_file(self):
-        print("file not exists!")
-
-    #msg is empty
-    def empty_obj(self):
-            print("msg is empty!")
+#msg is empty
+def empty_obj():
+        print("msg is empty!")
 
 def main():
     global PATH
-    PATH = 'E:\\WorkSpace\\Python_Selenium\\2.eml'
+    PATH = 'E:\WorkSpace\HelloWorld1.eml'
     print(PATH)
     msg = get_message(PATH)
     # print(msg)
@@ -109,7 +107,6 @@ def main():
     print('subject:{}'.format(get_subject(PATH)))
     print('#' * 50)
     print(parse_subject(msg))
-    print()
     print('#' * 50)
     print('from:{}'.format(get_from(msg)))
     print('to:{}'.format(get_to(msg)))
--- readEmail2.py
+++ readEmail2.py
@@ -1,7 +1,2 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
-import poplib
-from imaplib import IMAP4
-import email
-import os
-import ReadEmail
